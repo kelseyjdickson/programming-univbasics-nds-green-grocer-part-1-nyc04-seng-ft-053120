dfcfcc8654221865349344e3156fd30d061e3912
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-21 22:52:34 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,9 @@
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
+  binding.pry
   #
   # Consult README for inputs and outputs
+
 
 end
 
@@ -12,6 +14,3 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 end
-
-
-  
