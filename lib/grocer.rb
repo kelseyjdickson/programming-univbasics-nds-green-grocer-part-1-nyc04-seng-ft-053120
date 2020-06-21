1bf89b5f708d18f0fb81ec798600f2b426670b9f
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-21 22:58:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,5 +1,13 @@
-require 'pry'
+require "pry"
 
+
+grocery_shelf = [
+  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+  { :item => "SALSA", :price => 1.50, :clearance => false },
+  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
+]
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   # binding.pry
